19c42ddf302ae7066d182133cbd5b94d165dccd0
Allan <61694786+AllanVI9@users.noreply.github.com>
2021-09-16 12:20:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-var server = require('./config/server')
+var server = require("./config/server")
+ 
 var bancodedados = require('./config/bancodedados')
-
+ 
 var app = server.app
 var porta = server.porta
-
+ 
 var conexao = bancodedados.conexao
 var documentos = bancodedados.documentos
-
-app.set('view engine',"ejs")
-
-//abrir o form.ejs
-app.get('/',(req,res)=>{
+ 
+app.set('view engine', "ejs")
+ 
+//abrir o forms.ejs
+app.get('/', (req,res)=>{
     conexao()
     documentos.find()
-    .then((documentos)=>{
-         console.log(documentos)
-        res.render("form", documentos)
+    
+   .then((documentos)=>{
+    console.log(documentos)
+        res.render("form", {documentos})
     })
 })
-
-//gravar as informações do formulario form.ejs
+ 
+//gravar as informações do formulário form.ejs
 app.post('/gravar',(req,res)=>{
+ 
     var dados = req.body
-
     conexao()
-
+ 
     new documentos({
         mensagem:dados.mensagem,
         nome:dados.nome,
         cargo:dados.cargo
+ 
     }).save()
-    res.send('informações enviadas!')
+    //res.send('informações enviadas!')
+    res.redirect('/')
 })
-
+ 
 app.listen(porta)
