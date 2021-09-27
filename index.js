8a25e96983f90bdb10fb0fa91156ea33da3da882
Allan <61694786+AllanVI9@users.noreply.github.com>
2021-09-27 13:08:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-//servidor 
-var express = require("express")
+var express = require('express')
 var app = express()
-//config database com acesso ao mongoDBatlas
+ 
+//configuração do database com acesso ao mongoDBatlas
 var mongoose = require('mongoose')
 var conexao = ()=>{
-    var caminho = mongoose.connect('mongodb+srv://somenteleitura:ivaSomenteleitura9@fiap.9lwql.mongodb.net/mongoatlas?retryWrites=true&w=majority')
-    }
+    var caminho = mongoose.connect('mongodb+srv://awesome:ivaAwesome9@fiap.9lwql.mongodb.net/mongoatlas?retryWrites=true&w=majority')
+}
 var schema = mongoose.Schema
 var depoimentos = new schema({
     nome:String,
     cargo:String,
     mensagem:String
 })
-var documentos = mongoose.model('depoimentos',depoimentos)
-//fim das configurações do database
+var documentos = mongoose.model('depoimentos', depoimentos)
+// fim das configurações do database
+ 
 var porta = process.env.PORT || 4040
-// configurações 
-app.set("view engine",'ejs')
+ 
+app.set("view engine", "ejs")
+ 
+// config
+app.set("view engine", "ejs")
 app.use(express.static('./'))
+ 
 //rota para abrir o arquivo index.ejs
-app.get('/',(req,res)=>{
+app.get('/',(req, res)=>{
     conexao()
     documentos.find().limit(3).sort({'_id':-1})
     .then((documentos)=>{
-        console.log(documentos)
         res.render('index',{documentos})
     })
-    })
-//ligar o servidor 
+    
+})
+ 
+// ligar o servidor
 app.listen(porta)
-  
