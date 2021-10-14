573ee5abf0034807078da224638baef181bbfb6c
Allan <61694786+AllanVI9@users.noreply.github.com>
2021-10-14 11:33:58 -0300

    Update bancodedados.js

--- config/bancodedados.js
+++ config/bancodedados.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
 var conexao = ()=>{
-    var caminho =  mongoose.connect('mongodb+srv://mongodbuser:735024ph@fiap.z1kor.mongodb.net/mongoatlas?retryWrites=true&w=majority')
+    var caminho =  mongoose.connect('mongodb+srv://awesome:ivaAwesome9@fiap.9lwql.mongodb.net/mongoatlas?retryWrites=true&w=majority')
 }
 
 var schema = mongoose.Schema
